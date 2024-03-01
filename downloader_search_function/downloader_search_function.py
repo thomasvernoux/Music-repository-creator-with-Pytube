617bb621c85397e0415f6069c4631ce1fb553ff6
Thomas Vernoux <thomas@vernoux.be>
2024-03-01 13:26:51 +0100

    correct bug playlist downloader
    path error during downloading - resolved

--- downloader_search_function/downloader_search_function.py
+++ downloader_search_function/downloader_search_function.py
@@ -5,7 +5,7 @@ from pytube import Search
 import re
 
 
-playlist_name = "coups de coeur deezer avec search.txt"
+playlist_name = "z footing 32.txt"
 
 
 def remove_special_characters(input_string):
@@ -13,13 +13,13 @@ def remove_special_characters(input_string):
     input_string = input_string.encode('ascii', 'ignore').decode('ascii')
 
     # Supprimer les caractères spéciaux à l'aide d'une expression régulière
-    input_string = re.sub(r'[^\w\s.]', '', input_string)
+    input_string = re.sub(r'[^\w\s./\\]', '', input_string)
 
     return input_string
 
 def download_best_audio_from_search(song_name, folder_name):
     results = Search(song_name)
-    
+
     if len(results.results) > 0:
         yt = results.results[0]
         audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
@@ -33,22 +33,32 @@ def download_best_audio_from_search(song_name, folder_name):
 
             print(f"Downloading {yt.title}...")
 
-            # Create directory if it doesn't exist
-            if not os.path.exists('audio'):
-                os.makedirs('audio')
-            output_dir = os.path.join('audio', folder_name)
+            # Modifier le chemin de sortie pour le téléchargement et la conversion des fichiers audio
+            folder_name, extention = os.path.splitext(playlist_name.strip())
+            output_dir = os.path.join(os.getcwd(), f'audio\{folder_name}')
+            print("output dir : ", output_dir)
+
+
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
-
             # Download the audio stream to the 'audio' directory
             audio_path = audio_stream.download(output_path=output_dir)
             print(f"{yt.title} downloaded successfully.")
+            print("audio path : ", audio_path)
 
-            # Convert the downloaded audio file to MP3
+
+            
+            
+            # Convertir le fichier audio téléchargé en MP3
             mp4_audio = AudioFileClip(audio_path)
-            mp3_audio_path = remove_special_characters(os.path.splitext(audio_path)[0] + '.mp3')
+            mp3_audio_path = os.path.join(output_dir, f'audio_{yt.title}.mp3')
             mp4_audio.write_audiofile(mp3_audio_path)
             mp4_audio.close()
+
+            # Renommer le fichier MP3 sans les caractères spéciaux
+            new_mp3_audio_path = os.path.join(output_dir, f'audio_{remove_special_characters(yt.title)}.mp3')
+            os.rename(mp3_audio_path, new_mp3_audio_path)
+            
 
             # Delete the .webm file after conversion
             os.remove(audio_path)
@@ -63,9 +73,11 @@ def download_best_audio_from_search(song_name, folder_name):
 if __name__ == "__main__":
     with open('downloader_search_function/' + playlist_name, 'r') as file:
         lines = [line.strip() for line in file.readlines() if line.strip() and not line.startswith('#')]
-    
+
     for line in lines:
         download_best_audio_from_search(line, playlist_name)
+
+
 
 
 
