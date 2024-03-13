8a66f4297e906ad45dca2099fece42e09fa9745f
Thomas Vernoux <thomas@vernoux.be>
2024-03-13 11:23:55 +0100

    switch playlist downloader to class

--- downloader playlist/class.py
+++ downloader playlist/class.py
@@ -1,19 +1,59 @@
+from functions import *
+
 
 class Album:
-    def __init__(self):
+    def __init__(self, Artist = None):
+        self.artist = Artist
         self.name = None
         self.URL = None
         self.has_to_be_downloaded = None
+        self.folder_name = None
         return
+    
+    def download(self):
+        """
+        Download the album
+        """
+        
+        if self.has_to_be_downloaded :
+            self.folder_name = f"{self.artist}/{self.name}"
+            try : 
+                download_best_audio_playlist(self.URL, self.folder_name)
+                self.has_to_be_downloaded = False
+
+            except Exception as e:
+                print("Exception error:", e)
+            
 
 class Artist:
     def __init__(self):
-        self.artist_name = None
+        self.name = None
         self.albums_list = []
         return
 
-    
-def get_data(file):
+    def print_info(self):
+        """
+        Print in the terminal all artist info (including albums)
+        """
+        
+        print("Artist name : ", self.name)
+        for album in self.albums_list :
+            print("Album : ", album.name)
+
+    def download_music(self):
+
+        for album in self.albums_list :
+            album.download()
+            global MUSIC_DATA
+            write_music_data(MUSIC_DATA, path = "URL.txt")
+
+
+
+
+def get_music_data(file):
+    """
+    Return a list of Artist class
+    """
 
     ARTISTS_LIST = []               # list of Artists objects
 
@@ -27,29 +67,61 @@ def get_data(file):
 
         # Check if the line describe an artist
         if line.startswith("Artist :") :
+            artist_name = line.split(":")[1].strip()
             ARTISTS_LIST.append(Artist())
-            ARTISTS_LIST[-1].artist_name = line.split(":")[1].strip()
+            ARTISTS_LIST[-1].name = artist_name
+            print(artist_name)
             continue
 
         if line.startswith(" ") or line.startswith ("\n")  :
             continue
         
         
-        # The album has not to be downloaded
-        album = Album()
-        line_splited = line.replace("#", "").split(':', 1)
+        
+        album = Album(artist_name)
+        line_splited = line.replace("#", "").split(' : ', 1)
         album.name, album.URL = line_splited
         last_artist = ARTISTS_LIST[-1]
+        album.URL = album.URL.replace("\n", "")
         ARTISTS_LIST[-1].albums_list.append(album)
+
+        """
+        Debug
+        """
+        if album.name.startswith("Complete"):
+            a = 3
 
         # Check if the line start with '#'
         if line[0] == "#":
             album.has_to_be_downloaded = False
         else : 
             album.has_to_be_downloaded = True
-
     return ARTISTS_LIST
 
-a = get_data("urls copy 2.txt")
+def write_music_data(DATA, path = "URL.txt"):
 
-b = 3
+    file = open(path, 'w')
+
+    for Artist in DATA : 
+        file.write(f"Artist : {Artist.name}\n")
+        for Album in Artist.albums_list :
+            if Album.has_to_be_downloaded == False : 
+                file.write("#")
+            file.write(f"{Album.name} : {Album.URL}\n")
+        file.write(f"\n")
+    
+    file.close()
+
+
+path = "urls.txt"
+
+if __name__ == "__main__":
+
+    global MUSIC_DATA
+    MUSIC_DATA = get_music_data(path)
+    for artist in MUSIC_DATA : 
+        artist.download_music()
+
+    write_music_data(MUSIC_DATA, path = f"downloader playlist/{path}")
+        
+
---
+++ downloader playlist/functions.py
@@ -0,0 +1,84 @@
+
+
+from pytube import Playlist, YouTube  # Import the Playlist and YouTube classes from the pytube library
+from moviepy.editor import AudioFileClip  # Import the AudioFileClip class from the moviepy.editor library
+import os  # Import the os module for interacting with the operating system
+import re
+
+def remove_special_characters(input_string):
+    """
+    This function is used to transform a input string into output string without specials caracters
+    
+    """
+
+    # Remove emojis
+    input_string = input_string.encode('ascii', 'ignore').decode('ascii')
+
+    # Define allowed characters including slashes and periods
+    allowed_characters = r'[a-zA-Z0-9\s\\/.-]'
+
+    # Remove special characters using regex
+    input_string = re.sub(fr'[^{allowed_characters}]', '', input_string)
+
+    # remove '
+    input_string = re.sub("'", '', input_string)
+    
+    return input_string
+
+def download_best_audio_playlist(playlist_url, folder_name):
+    """
+    This function is used to download the best audio of a playlist tracks
+    input : 
+    @playlist URL : Youtube URL of the playlist
+    @folder name : download destination
+    """
+
+    playlist = Playlist(playlist_url)    # create the playlist object
+    for video in playlist.video_urls:  
+        yt = YouTube(video)              # create video object
+        audio_stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        if audio_stream:
+
+            # Generate a name for the song
+            file_name = f'{yt.title}.mp3'
+            
+            # Check if the audio file already exist
+            test_path = os.path.join('audio', folder_name, remove_special_characters(file_name))
+            exist  = os.path.exists(test_path)
+            if exist:
+                print(f"{file_name} already exists. Skipping...")
+                continue  # Go to the next song
+
+            print(f"Downloading {yt.title}...")
+            
+            # Create directory if it doesn't exist
+            if not os.path.exists('audio'):
+                os.makedirs('audio')
+            output_dir = os.path.join('audio', folder_name)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+
+
+            # Download the audio stream to the 'audio' directory
+            audio_path = audio_stream.download(output_path=output_dir)
+            print("audio path : ", audio_path)
+            print(f"{yt.title} downloaded successfully.")
+
+            # Convert the downloaded audio file to MP3
+            mp4_audio = AudioFileClip(audio_path)
+            mp3_audio_path = remove_special_characters(os.path.splitext(audio_path)[0] + '.mp3')
+            mp4_audio.write_audiofile(mp3_audio_path)
+            mp4_audio.close()
+
+            # Delete the .webm file after conversion
+            os.remove(audio_path)
+            print(f".webm file deleted for {yt.title}")
+
+            print(f"Conversion to MP3 completed for {yt.title}.")
+        
+        
+        else:
+            print(f"No audio stream available for {yt.title}.")
+
+
+
